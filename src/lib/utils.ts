3264548265c56c78cc4dadf53f37d74d50f97db7
mitreb <tmitreb@gmail.com>
2025-01-11 18:56:09 +0100

    Add downloadImage utility function for image downloading

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,18 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+import FileSaver from 'file-saver';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export async function downloadImage(id: string, url: string) {
+  FileSaver.saveAs(url, `download-${id}.jpg`);
+}
+
+export function generateImageId(prompt: string) {
+  const sanitizedPrompt = prompt.replace(/\s+/g, '_');
+  return `${sanitizedPrompt}-${Date.now()}-${Math.random()
+    .toString(36)
+    .slice(2, 11)}`;
 }
